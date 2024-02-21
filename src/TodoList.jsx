a44fe44b074afb2cb652da74dc9185df07e092e2
Ryan Magaya <2786978m@student.gla.ac.uk>
2024-02-21 20:59:04 +0000

    implemented hooks

---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem'; 
+
+export function TodoList({ todos , toggleTodo, deleteTodo}) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => (
+        <TodoItem {... todo} key={todo.id} toggleTodo= {toggleTodo} deleteTodo = {deleteTodo} />
+      ))}
+    </ul>
+  )
+}
